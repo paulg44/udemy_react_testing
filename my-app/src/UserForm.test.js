f486202d4f0a374d0661c9940a96c4135e2802f8
Paul Garton <paulgarton1984@hotmail.com>
2024-05-30 20:20:50 +0100

    start userList tests, fallback #1 data-testid

--- my-app/src/UserForm.test.js
+++ my-app/src/UserForm.test.js
@@ -16,7 +16,8 @@ test("it calls onUserAdd when the form is submitted", async () => {
   const mock = jest.fn();
   render(<UserForm onUserAdd={mock} />);
 
-  const [nameInput, emailInput] = screen.getAllByRole("textbox");
+  const nameInput = screen.getByRole("textbox", { name: /name/i });
+  const emailInput = screen.getByRole("textbox", { name: /email/i });
 
   await user.click(nameInput);
   await user.keyboard("jane");
