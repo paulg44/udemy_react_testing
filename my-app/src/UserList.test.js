217a118adfd4d206e6cc0880fb8c7484a19f897b
Paul Garton <paulgarton1984@hotmail.com>
2024-05-30 20:35:41 +0100

    make a function at top of test page with the rendered component and user list etc

--- my-app/src/UserList.test.js
+++ my-app/src/UserList.test.js
@@ -1,19 +1,37 @@
 import { render, screen, within } from "@testing-library/react";
 import UserList from "./userList";
 
-test("render one row per user", () => {
+function renderComponent() {
   const users = [
     { name: "jane", email: "jane@jane.com" },
     { name: "sam", email: "sam@sam.com" },
   ];
-  const { container } = render(<UserList users={users} />);
+  render(<UserList users={users} />);
 
-  //   const rows = within(screen.getByTestId("users")).getAllByRole("row");
+  return {
+    users,
+  };
+}
 
-  //   eslint-disable-next-line
-  const rows = container.querySelectorAll("tbody tr");
+test("render one row per user", () => {
+  renderComponent();
+
+  const rows = within(screen.getByTestId("users")).getAllByRole("row");
+
+  //   //   eslint-disable-next-line
+  //   const rows = container.querySelectorAll("tbody tr");
 
   expect(rows).toHaveLength(2);
 });
 
-test("render the email and name of each user", () => {});
+test("render the email and name of each user", () => {
+  const { users } = renderComponent();
+
+  for (let user of users) {
+    const name = screen.getByRole("cell", { name: user.name });
+    const email = screen.getByRole("cell", { name: user.email });
+
+    expect(name).toBeInTheDocument();
+    expect(email).toBeInTheDocument();
+  }
+});
