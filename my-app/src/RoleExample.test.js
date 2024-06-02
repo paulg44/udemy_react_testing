df225474b42f7613c29543e247c8c4087f65fb1c
Paul Garton <paulgarton1984@hotmail.com>
2024-06-02 10:56:11 +0100

    add roleExample function to test roles

---
+++ my-app/src/RoleExample.test.js
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react";
+
+function RoleExample() {
+  return (
+    <div>
+      <a href="/">Link</a>
+      <button>Button</button>
+      <footer>Contentinfo</footer>
+      <h1>Heading</h1>
+      <header>Banner</header>
+      <img alt="description" /> img
+      <input type="checkbox" /> checkbox
+      <input type="number" /> Spinbutton
+      <input type="radio" /> Radio
+      <input type="text" /> Textbox
+      <li>Listitem</li>
+      <ul>Listgroup</ul>
+    </div>
+  );
+}
+
+render(<RoleExample />);
+
+test("can find elements by role", () => {
+  render(<RoleExample />);
+
+  const roles = [""];
+});
