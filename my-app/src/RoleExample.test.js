05facd097d7630df7b37b493a454eae187fa79a4
Paul Garton <paulgarton1984@hotmail.com>
2024-06-02 11:10:30 +0100

    add a test and component for finding button based on accessible name

--- my-app/src/RoleExample.test.js
+++ my-app/src/RoleExample.test.js
@@ -44,6 +44,21 @@ test("can find elements by role", () => {
   }
 });
 
-// function AccessibleName() {
-//   retunr;
-// }
+function AccessibleName() {
+  return (
+    <div>
+      <button>Submit</button>
+      <button>Cancel</button>
+    </div>
+  );
+}
+
+test("can select by accessible name", () => {
+  render(<AccessibleName />);
+
+  const submitButton = screen.getByRole("button", { name: /submit/i });
+  const cancelButton = screen.getByRole("button", { name: /cancel/i });
+
+  expect(submitButton).toBeInTheDocument();
+  expect(cancelButton).toBeInTheDocument();
+});
