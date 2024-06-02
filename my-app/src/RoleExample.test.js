600d6be5012b987ce1389e612f2dade691a97549
Paul Garton <paulgarton1984@hotmail.com>
2024-06-02 11:26:27 +0100

    test icon buttons

--- my-app/src/RoleExample.test.js
+++ my-app/src/RoleExample.test.js
@@ -83,3 +83,26 @@ test("shows an email and search input", () => {
   expect(emailInput).toBeInTheDocument();
   expect(searchInput).toBeInTheDocument();
 });
+
+function IconButtons() {
+  return (
+    <div>
+      <button aria-label="sign in">
+        <svg />
+      </button>
+      <button aria-label="sign out">
+        <svg />
+      </button>
+    </div>
+  );
+}
+
+test("find elements based on label", () => {
+  render(<IconButtons />);
+
+  const signInButton = screen.getByRole("button", { name: /sign in/i });
+  const signOutButton = screen.getByRole("button", { name: /sign out/i });
+
+  expect(signInButton).toBeInTheDocument();
+  expect(signOutButton).toBeInTheDocument();
+});
